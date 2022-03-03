5b6d36b68a4c2561ce11a59221e433fa156189d0
Yun Jen,Hsu <soyuz18@gmail.com>
2022-03-03 14:40:59 +0800

    Sordfish dev start

---
+++ src/pages/SwordfishPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <swordfish></swordfish>
+  </div>
+</template>
+<script>
+
+
+import swordfish from "../components/swordfish/swordfish.vue";
+
+
+
+export default {
+  setup() {
+
+  },
+  components:{
+    swordfish
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
