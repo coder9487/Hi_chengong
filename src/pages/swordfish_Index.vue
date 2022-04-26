06bf06f76106c073a75f1d24ddf6f3d31f84b6a6
Yun Jen,Hsu <soyuz18@gmail.com>
2022-04-26 18:04:43 +0800

    Add swordfish challenge

---
+++ src/pages/swordfish_Index.vue
@@ -0,0 +1,101 @@
+<template>
+  <div id="IndexPageGroup">
+    <img id="StartBackgroung" src="images/challenge.png" v-show="hideFlag" />
+    <div
+      v-show="hideFlag"
+      class="start_btn text-h5 text-weight-bolder"
+      @click="videoFinish()"
+    >
+      開始體驗
+    </div>
+  </div>
+  <video
+    id="startUpVedio"
+    @ended="videoFinish"
+    @dblclick="videoDblcilck"
+    playsinline
+    preload
+  >
+    <source src="../../public/startupVideo.mp4" type="video/mp4" />
+  </video>
+  <div></div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageIndex",
+  setup() {
+    return {};
+  },
+  data() {
+    return {
+      hideFlag: true,
+      clickEnable: false,
+    };
+  },
+  methods: {
+    videoFinish() {
+      this.clickEnable = true;
+      this.$router.push("swordfishChallenge");
+    },
+    videoDblcilck() {},
+    playVideo() {
+      this.hideFlag = false;
+      var myVideo = document.getElementById("startUpVedio");
+      myVideo.play();
+    },
+  },
+});
+</script>
+<style lang="scss" scoped>
+#startUpVedio {
+  width: 100vw;
+  height: auto;
+  position: absolute;
+  z-index: 2;
+}
+
+.start_btn {
+  width: 15%;
+  height: 15%;
+  border-radius: 50px;
+  position: absolute;
+  bottom: 7vh;
+  left: 15vw;
+  border: 20px;
+  display: flex;
+  background-color: #F49D1A;
+  color: #ffffff;
+  z-index: 5;
+  text-align: center;
+  align-content: center;
+  align-items: center;
+  justify-content: center;
+  font-size: large;
+  transition: 1s;
+
+  @media screen and (min-width: 1024px) {
+    padding: 1%;
+    height: 10%;
+    font-size:x-large;
+  }
+
+  &:hover {
+    border-radius: 50px;
+    transition: 1s;
+    background-color: #FF7A00;
+    color: white;
+  }
+}
+
+#StartBackgroung {
+  z-index: 4;
+  width: 100vw;
+  height: 100vh;
+  overflow-y: hidden;
+  display: flex;
+  position: absolute;
+}
+</style>
