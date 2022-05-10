fecc18b24189ec07f9409980528ea6884417dd4c
Yun Jen,Hsu <soyuz18@gmail.com>
2022-05-10 17:29:48 +0800

    Fix router

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,45 +1,41 @@
-// import Market from 'src/pages/Market'
-// import DiningTablePage from 'src/pages/DiningTablePage'
-// import Swordfish from 'src/pages/Swordfish'
-// import Pisirian from 'src/pages/Pisirian'
 
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-
-      {path: '', component:import('src/pages/Index.vue')},
-      {path: '/swordfish_pre', component: () => import('src/pages/Lab/Swordfish3D_previous')},
-      {path: '/lab01', component: () => import('src/pages/Lab/lab01')},
-      {path: '/DiningTable', component: () => import('pages/DiningTablePage.vue')},
-      {path: '/Pisirian', component: () => import('src/pages/Pisirian.vue')},
-      {path: '/Market', component: () => import('src/pages/Market.vue')},
-      {path: '/Swordfish', component: () => import('src/pages/Swordfish.vue')},
-      // {path: '/DiningTable', component: DiningTablePage},
-      // {path: '/Pisirian', component: Pisirian},
-      // {path: '/Market', component: Market},
-      // {path: '/Swordfish', component: Swordfish},
-      {path: '/Final', component: () => import('src/pages/Final.vue')},
-      {path: '/swordfishChallenge', component: () => import('src/pages/swordfishChallenge.vue')},
-      {path: '/Challenge', component: () => import('src/pages/swordfish_Index.vue')},
-      {path: '/Member', component: () => import('src/pages/Member.vue')},
-      {path: '/MarketVR', component: () => import('src/pages/MarketVR.vue')},
-      {path: '/Sensor', component: () => import('src/pages/Sensor.vue')},
-
-
-
-
-
-    ]
+      { path: "", component: ()=>import("pages/Index.vue") },
+      {
+        path: "/DiningTable",
+        component: () => import("pages/DiningTablePage.vue"),
+      },
+      { path: "/Pisirian", component: () => import("src/pages/Pisirian.vue") },
+      { path: "/Market", component: () => import("src/pages/Market.vue") },
+      {
+        path: "/Swordfish",
+        component: () => import("src/pages/Swordfish.vue"),
+      },
+      { path: "/Final", component: () => import("src/pages/Final.vue") },
+      {
+        path: "/swordfishChallenge",
+        component: () => import("src/pages/swordfishChallenge.vue"),
+      },
+      {
+        path: "/Challenge",
+        component: () => import("src/pages/swordfish_Index.vue"),
+      },
+      { path: "/Member", component: () => import("src/pages/Member.vue") },
+      { path: "/MarketVR", component: () => import("src/pages/MarketVR.vue") },
+      { path: "/Sensor", component: () => import("src/pages/Sensor.vue") },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/Error404.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/Error404.vue"),
+  },
+];
 
-export default routes
+export default routes;
